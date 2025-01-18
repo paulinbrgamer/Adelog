00217d8230652ed276ac9baf212c248a5fc4acc3
oPaulin <pauloferadosgames@gmail.com>
2025-01-18 13:25:11 -0300

    adicionado funcionalidade atualização das unidades no banco de dados

--- src/pages/SellScreen.jsx
+++ src/pages/SellScreen.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useState } from 'react'
 import { useApp } from './AppProvider'
 import ProductComponent from '../components/ProductComponent'
 import styled from 'styled-components'
 import Container from '../components/Container'
 import IconButton from '../components/IconButton'
+import ModalComponent from '../components/ModalComponent'
+import { supabase } from "../services/cliente";
 const Title = styled.p`
   font-weight: 600;
   font-size: 14pt ;
@@ -16,30 +18,53 @@ const Products = styled.div`
   box-sizing: border-box;
 `
 export default function SellScreen() {
-  const {Cart,setCart} = useApp()
-
-  const handleCancel=()=>{
-      setCart([])
+  const { Cart, setCart,storeData,setStore } = useApp()
+  const [isFinished, setisFinished] = useState(false)
+  const [isAproved,setisAproved] = useState(false)
+  const handleCancel = () => {
+    setCart([])
   }
+  const handleFinalize = async() => {
+    const update = Cart.map((item)=>{
+      const current = storeData.filter(data=>data.id==item.id)
+      const newItem = {id:item.id,units:current[0].units-item.units}
+      return newItem
+    })    
+    update.forEach(async (item )=> {
+      const {data,error} = await supabase.from('products').update({units:item.units}).eq('id',item.id)
+      if (error) {
+        console.log('Erro : ',error);
+      }
+      else{
+        setisAproved(true)
+      }
+    });
+  }
+
   return (
     <Container border={'none'} height={'calc(100% - 140px)'}>
-        <Title>Items: {Cart.length}</Title> 
+      {isFinished ?
+        <ModalComponent>
+          Teste
+        </ModalComponent> : null
+      }
+      <Title>Items: {Cart.length}</Title>
       <Products>
-        {Cart.map((item)=>
-          <ProductComponent trash key={item.id+'cart'} data={item}/>
+        {Cart.map((item) =>
+          <ProductComponent trash key={item.id + 'cart'} data={item} />
         )}
       </Products>
-      {Cart.length>0?<div style={{display:'flex',flexDirection:"column",justifyContent:"space-evenly",padding:"6px"}}>
-      <Title>Total: R$ {Cart.reduce((acc,obj)=>acc+=obj.price,0).toFixed(2)}</Title>
-        <div style={{display:'flex',justifyContent:"space-evenly",padding:"6px"}}>
-        <IconButton style={{padding:'4px'}} onclick={()=>handleCancel()}>
-          <p style={{ fontWeight:'normal', fontSize: "12pt" }}>Cancelar</p>
-        </IconButton>
-        <IconButton style={{padding:'4px'}}>
-          <Title>Finalizar</Title>
-        </IconButton>
+      {Cart.length > 0 ? <div style={{ display: 'flex', flexDirection: "column", justifyContent: "space-evenly", padding: "6px" }}>
+        <Title>Total: R$ {Cart.reduce((acc, obj) => acc += obj.price, 0).toFixed(2)}</Title>
+        <div style={{ display: 'flex', justifyContent: "space-evenly", padding: "6px" }}>
+          <IconButton style={{ padding: '4px' }} onclick={() => handleCancel()}>
+            <p style={{ fontWeight: 'normal', fontSize: "12pt" }}>Cancelar</p>
+          </IconButton>
+          <IconButton onclick={() => handleFinalize()} style={{ padding: '4px' }}>
+            <Title>Finalizar</Title>
+          </IconButton>
         </div>
-      </div>:null}
+      </div> : null}
     </Container>
   )
 }
