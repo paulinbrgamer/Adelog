0f4be70aa1c0aade97d11a57df6e8dcca79c017a
paulinbrgamer <pauloferadosgames@gmail.com>
2025-01-23 18:26:47 -0300

    creating vite api url

--- src/services/cliente.js
+++ src/services/cliente.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-const SUPABASEURL = process.env.REACT_APP_SUPABASE_URL;
-const KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const SUPABASEURL = import.meta.env.VITE_API_URL ;
+const KEY = import.meta.env.VITE_API_KEY;
 export const supabase = createClient(SUPABASEURL,KEY)
