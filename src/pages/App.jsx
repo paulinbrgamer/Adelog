63113bafd7c2f5c11dd5f86895f935cd6e13c388
paulinbrgamer <pauloferadosgames@gmail.com>
2025-01-25 08:41:44 -0300

    formatação do component App

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,45 +1,64 @@
 import NavOptions from '../components/NavOptions'
 import IconButton from '../components/IconButton'
 import { useState } from 'react'
-import { PackageSearch, User, FileText  , ShoppingCart } from 'lucide-react';
+import { PackageSearch, User, FileText, ShoppingCart } from 'lucide-react';
 import UserScreen from './UserScreen'
 import SellScreen from './SellScreen'
 import ProductsScreen from './ProductsScreen'
 import { AppProvider } from './AppProvider';
 import SalesComponent from '../pages/SalesComponent'
-import {AppPage} from '../components/styled/AppPage'
+import { AppPage } from '../components/styled/AppPage'
 
 function App() {
   const [currentRendering, setcurrentRendering] = useState('user')
-  const title = {user:'Dados pessoais',products:'Estoque de Produtos',sell:"Carrinho de compras",sales:"Histórico de vendas"}
+  const title = { user: 'Dados pessoais', products: 'Estoque de Produtos', sell: "Carrinho de compras", sales: "Histórico de vendas" }
   const handleChangeScreen = (setRender, newRender) => {
     setRender(newRender)
   }
-  
+  const ChangeStyleNav = (current, target) => current == target ? 'black' : 'white'
   return (
     <AppProvider>
       <AppPage  >
-        <h3 style={{padding:"10px"}}>{title[currentRendering]}</h3>
-        {currentRendering === 'user' ? <UserScreen /> : currentRendering === 'sell' ? <SellScreen /> :currentRendering==='products'? <ProductsScreen />:<SalesComponent/>}
-        
+        <h3 style={{ padding: "10px" }}>{title[currentRendering]}</h3>
+        {currentRendering === 'user' ? <UserScreen /> : currentRendering === 'sell' ? <SellScreen /> : currentRendering === 'products' ? <ProductsScreen /> : <SalesComponent />}
+
         <NavOptions>
           <IconButton onclick={() => handleChangeScreen(setcurrentRendering, 'user')}>
-            <User size={26} strokeWidth={1} fill={currentRendering=='user'?'black':'white'} color={currentRendering=='user'?'black':'gray'} />
-            <p style={{color:currentRendering=='user'?'black':'gray'}}>Conta</p>
+            <User
+              size={26}
+              strokeWidth={1}
+              stroke={'gray'}
+              fill={ChangeStyleNav(currentRendering, 'user')}
+              color={currentRendering == 'user' ? 'black' : 'gray'} />
+            <p style={{ color: currentRendering == 'user' ? 'black' : 'gray' }}>Conta</p>
           </IconButton>
 
           <IconButton onclick={() => handleChangeScreen(setcurrentRendering, 'products')}>
-            <PackageSearch size={26} strokeWidth={1} stroke={currentRendering=='products'?'white':'gray'} fill={currentRendering=='products'?'black':'white'} color={currentRendering=='products'?'black':'gray'} />
-            <p style={{color:currentRendering=='products'?'black':'gray'}}>Produtos</p>
+            <PackageSearch
+              size={26}
+              strokeWidth={1}
+              stroke={'gray'}
+              fill={ChangeStyleNav(currentRendering, 'products')} color={currentRendering == 'products' ? 'black' : 'gray'} />
+            <p style={{ color: currentRendering == 'products' ? 'black' : 'gray' }}>Produtos</p>
           </IconButton>
 
           <IconButton onclick={() => handleChangeScreen(setcurrentRendering, 'sell')}>
-            <ShoppingCart size={26} stroke={currentRendering=='sell'?'black':'gray'} strokeWidth={1.3} fill={currentRendering=='sell'?'black':'white'} color={currentRendering=='sell'?'black':'gray'}/>
-            <p style={{color:currentRendering=='sell'?'black':'gray'}}>Carrinho</p>
+            <ShoppingCart
+              size={26}
+              stroke={'gray'}
+              strokeWidth={1.3}
+              fill={ChangeStyleNav(currentRendering, 'sell')}
+              color={currentRendering == 'sell' ? 'black' : 'gray'} />
+            <p style={{ color: currentRendering == 'sell' ? 'black' : 'gray' }}>Carrinho</p>
           </IconButton>
           <IconButton onclick={() => handleChangeScreen(setcurrentRendering, 'sales')}>
-            <FileText  size={26} stroke={currentRendering=='sales'?'black':'gray'} strokeWidth={1.3} fill={currentRendering=='sales'?'black':'white'} color={currentRendering=='sales'?'black':'gray'}/>
-            <p style={{color:currentRendering=='sales'?'black':'gray'}}>Histórico</p>
+            <FileText
+              size={26}
+              stroke={'gray'}
+              strokeWidth={1.3}
+              fill={ChangeStyleNav(currentRendering, 'sales')}
+              color={currentRendering == 'sales' ? 'black' : 'gray'} />
+            <p style={{ color: currentRendering == 'sales' ? 'black' : 'gray' }}>Histórico</p>
           </IconButton>
         </NavOptions>
       </AppPage>
