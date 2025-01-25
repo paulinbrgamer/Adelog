b1fe7fc633bacc88c89bf7052efc75803edece50
paulinbrgamer <pauloferadosgames@gmail.com>
2025-01-25 08:18:33 -0300

    mudança da implementação do toast para função

--- src/auth/Authprovider.jsx
+++ src/auth/Authprovider.jsx
@@ -2,32 +2,30 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 import { supabase } from "../services/cliente";
 import { useNavigate } from "react-router-dom";
 const AuthContext = createContext()
-
-export const AuthProvider = ({children})=>{
-    const  [User, setUser] = useState(null)
-    
+export const AuthProvider = ({ children }) => {
+    const [User, setUser] = useState(null)
     const navegate = useNavigate()
-
-    const login = async(userData,settoast)=>{
-        const {data,error} = await supabase.from('users').select('*').eq('key',`${userData.key}`).single()
-        if (error){
-            if(settoast){
-                settoast(true)
-                setTimeout(() => {
-                settoast(false)
-                }, 1500)
+    const showToast = (setToastVisible) => {
+        setToastVisible(true);
+        setTimeout(() => setToastVisible(false), 1500); // Exibe por 3 segundos
+    };
+    const login = async (userData, settoast) => {
+        const { data, error } = await supabase.from('users').select('*').eq('key', `${userData.key}`).single()
+        if (error) {
+            if (settoast) {
+                showToast(settoast)
             }
-            console.log('Erro ao buscar os dados: ',error);
-            setUser({user:false})
+            console.log('Erro ao buscar os dados: ', error);
+            setUser({ user: false })
         }
-        else{
+        else {
             navegate('/home')
             setUser(data)
-            localStorage.setItem('user',JSON.stringify(data)) 
-            
+            localStorage.setItem('user', JSON.stringify(data))
+
         }
     }
-    const logout = ()=>{
+    const logout = () => {
         setUser(null)
         localStorage.removeItem('user')
         navegate('/')
@@ -36,18 +34,17 @@ export const AuthProvider = ({children})=>{
         const session = JSON.parse(localStorage.getItem('user'))
         if (!session) {
             navegate('/')
-          }
-        else{
-            login({'key':session.key},null)
-          }
+        }
+        else {
+            login({ 'key': session.key }, null)
+        }
     }, [])
-    const values = {User,login,logout}
-
-    return(
+    const values = { User, login, logout }
+    return (
         <AuthContext.Provider value={values}>
             {children}
         </AuthContext.Provider>
     )
 
 }
-export const useAuth=()=>useContext(AuthContext)
+export const useAuth = () => useContext(AuthContext)
