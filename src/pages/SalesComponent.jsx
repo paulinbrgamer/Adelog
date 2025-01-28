e1507de46184b86efb7d11db977db26d0427d8c5
paulinbrgamer <pauloferadosgames@gmail.com>
2025-01-28 09:46:42 -0300

    fix do SalesComponent

--- src/pages/SalesComponent.jsx
+++ src/pages/SalesComponent.jsx
@@ -4,40 +4,39 @@ import styled from "styled-components"
 import { supabase } from "../services/cliente"
 import TableComponent from "../components/TableComponent"
 import { useApp } from "./AppProvider"
-import  Container  from "../components/styled/Container"
-const Title = styled.p`
-font-weight: 600;
-font-size: 12pt ;
-    `
+import Container from "../components/styled/Container"
+const ContainerStyle = {
+    overflow: 'scroll',
+    height: "calc(100% - 160px)"
+}
 
-const SalesComponent = ()=>{
-    const {User} = useAuth()
-    const {storeData} = useApp()
-    const [sales,setSales] = useState([])
+const SalesComponent = () => {
+    const { User } = useAuth()
+    const { storeData } = useApp()
+    const [sales, setSales] = useState([])
     useEffect(() => {
-        const fetchSales = async ()=>{
-            const {data,error} = await supabase.from('sales').select("*").eq(User?.permission=='adm'?'store_id':'user_id',User?.permission=='adm'?User.store_id:User?.id)
-            if(error){
-                console.log('Error : ',error)
-            }else{
+        const fetchSales = async () => {
+            const { data, error } = await supabase.from('sales').select("*").eq(User?.permission == 'adm' ? 'store_id' : 'user_id', User?.permission == 'adm' ? User.store_id : User?.id)
+            if (error) {
+                console.log('Error : ', error)
+            } else {
                 const SortedDate = data.sort((a, b) => new Date(b.date) - new Date(a.date))
-                setSales(SortedDate.map(item=>{
+                setSales(SortedDate.map(item => {
                     const date = new Date(item.date).toLocaleString()
-                    const nameItem = storeData.filter((product)=>item.id_product==product.id)
-                    item.id_product = nameItem[0]?.name 
+                    const nameItem = storeData.filter((product) => item.id_product == product.id)
+                    item.id_product = nameItem[0]?.name
                     item.date = date
                     return item
                 }))
-                
+
             }
         }
         fetchSales()
-    }, [User,storeData])
-    
+    }, [User, storeData])
+
     return (
-        <Container shadow = {'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'} border={'none'} height={'calc(100% - 160px)'} style={{overflow:'scroll'}} aligh={'none'}>
-            
-            <TableComponent data={sales} header={['Nome','Unidades','Total(R$)','Data']}/>
+        <Container style={ContainerStyle} >
+            <TableComponent data={sales} header={['Nome', 'Unidades', 'Total(R$)', 'Data']} />
         </Container>
     )
 }
